e2ff9929a5904d0c907c0ceef4f003e41cd31e7f
Paul Zaich <pzaich@gmail.com>
2025-04-23 23:20:26 -0700

    Fix url in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "tourpods.com" }
-  Rails.application.routes.default_url_options[:host] = "tourpods.com"
+  Rails.application.routes.default_url_options[:host] = "api.tourpods.com"
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
